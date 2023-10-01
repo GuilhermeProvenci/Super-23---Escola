c8d8246a8c28a48d9832524bf8c4c2e0178a87cf
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-10-01 00:17:58 -0300

    Refatoração

--- adm/cad_produtos.php
+++ adm/cad_produtos.php
@@ -6,16 +6,18 @@
 <body>
     <h1>Cadastro de Produtos</h1>
     <?php
-include("funcoes.php");
-include("../conexao.php");
-$nomeTabela = "produtos";
-$camposDesejados = array();
-$camposNaoDesejados = array(); 
-$camposReadonly = array("CodProduto");
-criarFormularioCadastro($nomeTabela, $conexaoid, $camposDesejados, $camposNaoDesejados, $camposReadonly);
-?>
+    include("funcoes.php");
+    include("../conexao.php");
+    $nomeTabela = "produtos";
+    $camposDesejados = array();
+    $camposNaoDesejados = array(); 
+    $camposReadonly = array("CodProduto");
+    criarFormularioCadastro($nomeTabela, $conexaoid, $camposDesejados, $camposNaoDesejados, $camposReadonly);
+    ?>
 
-<br>
-<a href="./opcoes.html">Voltar para a página inicial</a>
+    <br>
+    <a href="./opcoes.html">Voltar para a página inicial</a>
+
+    
 </body>
 </html>
