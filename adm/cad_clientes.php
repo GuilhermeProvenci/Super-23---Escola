a3672b6b1dc1976bbfb606c4922d189ebfebf3d1
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-10-09 21:53:13 -0300

    Estilização - CSS

--- adm/cad_clientes.php
+++ adm/cad_clientes.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Cadastro de Usuários</title>
     <link rel="stylesheet" type="text/css" href="../css_avulsos/cadastros.css" />
 </head>
+
 <body>
     <h1>Cadastro de Usuários</h1>
     <?php
-        include("../conexao.php");
-        include("funcoes.php");
-        $nomeTabela = "clientes";
-        $camposDesejados = array();
-        $camposNaoDesejados = array("Validado"); 
-        $camposReadonly = array("CodCliente");
-        criarFormularioCadastro($nomeTabela, $conexaoid, $camposDesejados, $camposNaoDesejados, $camposReadonly);
+    include("../conexao.php");
+    include("funcoes.php");
+    $nomeTabela = "clientes";
+    $camposDesejados = array();
+    $camposNaoDesejados = array("Validado");
+    $camposReadonly = array("CodCliente");
+    criarFormularioCadastro($nomeTabela, $conexaoid, $camposDesejados, $camposNaoDesejados, $camposReadonly);
     ?>
     <br>
     <a href="./opcoes.html">Voltar para a página inicial</a>
 </body>
-</html>
 
+</html>
--- adm/lista_clientes.php
+++ adm/lista_clientes.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Clientes</title>
-    <link rel="stylesheet" type="text/css" href="../css_avulsos/lista-registros.css" /> 
+    <link rel="stylesheet" type="text/css" href="../css_avulsos/lista-registros.css" />
 </head>
+
 <body>
     <header>
         <h1>Lista de Clientes</h1>
@@ -15,6 +17,7 @@
         include("../conexao.php");
         include("funcoes.php");
         $opcoesPersonalizadas = array("Validar", "Editar", "Excluir");
+
         listarRegistros("clientes", $conexaoid, $opcoesPersonalizadas);
         ?>
     </main>
@@ -23,4 +26,5 @@
         <a href="./opcoes.html">Voltar para a página inicial</a>
     </footer>
 </body>
+
 </html>
