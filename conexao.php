4dda77908150b11eb92a0138fd8033ed399a342f
Guilherme Lima <guilhermelimamp2020@gmail.com>
2023-12-02 20:15:13 -0300

    Reestruturação

--- conexao.php
+++ conexao.php
@@ -1,14 +1,12 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$banco = "escola";
-$senha = "";
+require_once 'config.php';
 
-// Conectar usando MySQLi
-$conexaoid = new mysqli($host, $usuario, $senha, $banco);
+$conexaoid = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
-// Verificar a conexão
 if ($conexaoid->connect_error) {
     die("Não foi possível conectar ao banco de dados: " . $conexaoid->connect_error);
 }
+
+// Outras configurações para segurança
+$conexaoid->set_charset(DB_CHARSET);
 ?>
